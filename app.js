c61f268f6fa36d224f1d88ad2542c00328ef40c5
Nemozikr <ebhomrah@gmail.com>
2025-06-16 13:49:14 +0100

    wiped main file from tokeniser and search logic

--- app.js
+++ app.js
@@ -2,47 +2,15 @@ const fs = require('node:fs');
 const process = require('node:process');
 const readline = require('readline');
 
+const Tokenise = require('./components/tokeniser');
+const Search = require('./components/cli-complex');
+
 const args = process.argv.splice(2);
+
 
 if (args[0] !== 'complex') throw Error(`${args[0]} is not a complex command`);
 
-const search = {
-    fileExt: '.cx',
-    i: 0,
-    j: this.i + 3,
-    previous: null,
-    current: null,
-    foundFile: false,
-    fileName: null,
-    reset() {
-        search.i = 0; 
-        search.j = search.i + 3;
-        search.previous = search.current
-        search.current = null;
-    },
-    next() {
-        search.i++;search.j++;
-    },
-    set(value) {
-        search.previous = search.current;
-        search.current = value;
-    }
-
-}
-for (let arg = 0; arg < args.length; arg++) {
-    const argStr = args[arg]
-    for (;search.j <= argStr.length;) {
-        search.set(argStr.slice(search.i, search.j));
-        if (search.current == search.fileExt) {
-            search.foundFile = true
-            search.fileName = argStr;
-        }
-        search.next()
-    }
-    search.reset(this)
-}
-if (!search.foundFile) throw Error("No executables found");
-
+Search(args);
 
 console.log(`Initialising ${search.fileName}\n`)
 const reader = readline.createInterface({
@@ -56,47 +24,9 @@ const Complex = {
     state: []
 }
 
-const type = ['let', 'const'];
-const sign = ['+', '-'];
-const symbol = [';'];
-
-function evaluate(line) {
-    if (line[3] === '=') {
-        const val = line[4].replace(';', '');
-        Complex.state.push({
-            type: line[0],
-            name: line[1],
-            value: val,
-        });
-    }
-}
-
-
-function readUnderstand(line) {
-    let i = 0;
-    let current = '';
-    let statement = {
-        name: undefined,
-        type: undefined,
-    }
-    do {
-        // Read until ';' or end of line;
-        current += line[i];
-
-        if (type.includes(current.trim())) {
-            statement.type = current.trim();
-            current = '';
-        }
-        if (current === '=') {
-            statement.name = current.trim()
-        }
-        i++;
-    }while ( i < line.length );
-}
-
 reader.on('line', function (data) {
     Complex.process.line++;
-    const line = data.split('');
+    const line = data.split();
     //console.log(parts)
     readUnderstand(line)
     
