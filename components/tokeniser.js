d5c94758124470688d00c4b2be8c011771acba6c
Nemozikr <ebhomrah@gmail.com>
2025-06-18 11:36:11 +0100

    Modified Tokeniser to a fully working state, splits into tokens everything in app.cmpx correctly

--- components/tokeniser.js
+++ components/tokeniser.js
@@ -1,10 +1,15 @@
 const patterns = [
-    {   type: "WHITESPACE", regex: /^\s+/   },
-    {   type: "NUMBER", regex: /^\d+(\.\d+)?/  },
-    {   type: "STRING", regex: /^"(?:\\.|[^"\\])*"|^'(?:\\.|[^'\\])*'/   },
-    {   type: "KEYWORD", regex: /^(let|const|state|Set|zone|dropIf|complex|merge|skip|mergeskip|eventzone|on|off)/  },
-    {   type: "IDENTIFIER", regex: /^\b[a-zA-Z_][a-zA-Z0-9_]*\b/   },
-    {   type: "SYMBOL", regex: /^(==|!=|<=|>=|=>|::|&&|\|\||[;{}()\[\]=+\-*/.,])/   },
+    {   type: "WHITESPACE", regex: /^(\s+)/   },
+    {   type: "NUMBER", regex: /^(\d+(\.\d+)?)/  },
+    {   type: "DOUBLE_QUOTES", regex: /^("(?:\\.|[^"\\])*")/   },
+    {   type: "SINGLE_QUOTES", regex: /^('(?:\\.|[^'\\])*)'/  },
+    {   type: "MODE_KEYWORD", regex: /^(Set|optimised|complex|burst)/},
+    {   type: "STARTERS", regex: /^(let|const|state|eventzone|zone|mergeskip|merge|skip)/  },
+    {   type: "KEYWORDS", regex: /^(dropIf|on|off)/},
+    {   type: "IDENTIFIER", regex: /^(\b[a-zA-Z_][a-zA-Z0-9_]*\b)/   },
+    {   type: "BOOL_LOGIC", regex: /^(==|!=|<=|>=|=>|::|&&|\|\|)/   },
+    {   type: "SYMBOLS", regex: /^([;{}()\[\]=+\-*/\.,])/ },
+    {   type: "UNKNOWN", regex: /^(w|d|\s)+/ }
 ];
 
 function Tokenise(file) {
@@ -13,21 +18,21 @@ function Tokenise(file) {
     let nll = 0
     while (source.length > 0) {
         for (const {type, regex} of patterns) {
-            const match = regex.exec(source);
-            if (type === "WHITESPACE") break;
+            const match = regex.exec(source);  
             if (match) {
-                tokens.push({   type,   value: match[0] })
-                source = source.slice(match[0].length);
-                console.log(tokens)
-                console.log(source)
                 nll = 0;
-                break;
+                if (type) {
+                    tokens.push({   type,   value: match[0] })
+                    source = source.slice(match[0].length);
+                    break;
+                }
             }
+            
             
             if (match === null) {
                 nll++;
-                if (nll > 10) {
-                    throw new Error("Lexer stuck: unable to match next token in source: " + source.slice(0, 20));
+                if (nll >= 10) {
+                    throw new Error("Lexer stuck: unable to match next token in source: " + source.slice(0, 25));
                 }
             }
         }
